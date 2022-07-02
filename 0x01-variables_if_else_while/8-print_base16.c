029e948bec607c45dddc0a033dfa321bf975894b
leenduati <leenduati21@gmail.com>
2022-07-02 13:54:15 -0700

    Print characters to base 16 in c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a < 10; a++)
+		putchar(a + '0');
+	for (b = 'a'; b < 'g'; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
