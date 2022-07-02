0bd0821e869edf8bd0f04d966e3d890a4dc0f0fd
leenduati <leenduati21@gmail.com>
2022-07-02 12:30:29 -0700

    Code that prints alphabet and skips 2 letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	/* Program */
+	for (a = 'a'; a <= 'z'; a++)
+		if (!(a == 'q' || a == 'e'))
+			putchar(a);
+	putchar('\n');
+	return (0);
+}
