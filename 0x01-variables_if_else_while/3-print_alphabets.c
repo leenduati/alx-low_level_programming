28bff27344d25f5e320e3dcf8f6fb7a8cc4d0b78
leenduati <leenduati21@gmail.com>
2022-07-02 12:07:22 -0700

    Print out the alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0 indicates success
+ */
+
+int main(void)
+{
+	char a;
+	/* Program */
+	for (a = 'a'; a <= 'Z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
