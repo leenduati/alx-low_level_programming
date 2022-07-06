004fe4e4477b3687452ee93b494657664d610ea7
leenduati <leenduati21@gmail.com>
2022-07-06 00:02:42 -0700

    Edit 1 of Check if number is zero, or greater than 0, or less than 0

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,9 +10,18 @@
 int print_sign(int c)
 {
 	if (c == 0)
+{
+		_putchar('0');
 		return (0);
+}
 	else if (c > 0)
+{
+		_putchar('+');
 		return (1);
+}
 	else
+{
+		_putchar('-');
 		return (-1);
+}
 }
