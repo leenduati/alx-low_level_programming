ab0eab3d970c89335357d64e0873ebc409a3d4a6
leenduati <leenduati21@gmail.com>
2022-07-07 18:32:30 +0000

    Check if the parameter is uppercase, and return 1 else 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _isupper -Prints 1 is c is uppercase, else 0
+ *@c : parameter passed*
+ * Return: Always a (Success)
+ */
+
+
+int _isupper(int c)
+{
+	char a;
+
+	a = 0;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		a = 1;
+		_putchar(a);
+	}
+	else
+	{
+		a = 0;
+		_putchar(a);
+	}
+	return (a);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef main_H
+#define main_H
+
+/**
+*File: main.h
+*
+*descriptio: Header file containing declarations for all function
+*/
+
+int _putchar (char c);
+int _isupper(int a);
+
+
+#endif /* MAIN_H */
