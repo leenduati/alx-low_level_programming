3f8658aa550b1f6442fc012a449688a0ee210ad0
leenduati <leenduati21@gmail.com>
2022-07-07 14:29:23 +0000

    Fixed betty for leap year code

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -25,10 +25,10 @@ void print_remaining_days(int month, int day, int year)
 	else
 	{
 		if ((month == 2 && day == 60) && (year % 400 == 0 || year % 4 == 0) && (year % 100 == 0))
-                {
-                        printf("Day of the year: %d\n", day);
-                        printf("Remaining days: %d\n", 366 - day);
-                }
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
+		}
 
 		else if ((month == 2 && day == 60))
 		{
