a46ecdbd2416545b2b7604d46d8f63c44b02ad81
leenduati <leenduati21@gmail.com>
2022-07-07 21:24:35 +0000

    FizzBuzz in C

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main -Prints 1 is c is uppercase, else 0
+ *
+ * Return: Always a (Success)
+ */
+
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 5 == 0 && a % 3 == 0)
+			printf("FizzBuzz ");
+		else if (a % 5 == 0)
+			printf("Buzz ");
+		else if (a % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", a);
+	}
+	printf("\n");
+	return (0);
+}
