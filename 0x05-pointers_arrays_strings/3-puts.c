c490b15dedf28717c1d073924e19cbdb77dfa155
leenduati <leenduati21@gmail.com>
2022-07-11 20:20:36 +0000

    Used putchar for same output

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,17 +2,21 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * _puts- prints out a string as given
+ *@str : parameter passed*
+ * Return: Always a (Success)
+ */
 
 void _puts(char *str)
 {
 	int a;
 	int s;
+
 	s = strlen(str);
-
-
 	for (a = 0; a <= s; a++)
 	{
-		printf("%c", str[a]);
+		_putchar(str[a]);
 	}
-	printf("\n");
+	_putchar('\n');
 }
