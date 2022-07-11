2feac6e22272cb82c88e72d1b94377c40cf4c0a2
leenduati <leenduati21@gmail.com>
2022-07-11 18:58:43 +0000

    function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98-Reassigns the value of the addressto 8
+ *@n : parameter passed*
+ * Return: Always a (Success)
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef main_H
+#define main_H
+
+/**
+*File: main.h
+*
+*descriptio: Header file containing declarations for all function
+*/
+
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
