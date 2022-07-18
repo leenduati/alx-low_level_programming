766e8b525aff1fcc381bec470692996f46af9736
leenduati <leenduati21@gmail.com>
2022-07-18 19:09:49 +0000

    Fixed betty in 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,14 +3,22 @@
 #include <stdio.h>
 
 
+/**
+ *_strchr -  Returns a pointer to the first occurrence of c
+ *@s: A pointer to an char that will be updated from src
+ *@c: A char that will be copied
+ *Return: res
+ */
+
+
 char *_strchr(char *s, char c)
 {
 	int a;
 	char *res;
+
 	a = 0;
 
-
-	while(!(s[a] == '\0'))
+	while (!(s[a] == '\0'))
 	{
 		if (s[a] == c)
 		{
