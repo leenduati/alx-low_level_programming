d87dd8fe357ac33229a6ecb75631dd9f1db8cf51
leenduati <leenduati21@gmail.com>
2022-07-18 10:08:39 +0000

    README.ms added plus a program that fills the first n bytes of memory s with constant byte bb

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *_memset -  fills first n bytes of the mempry area pointed to by s
+ *@s: A pointer to an char that will be updated
+ *@b: A char that will be updated
+ *@n: value
+ *Return: 0
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+	return (0);
+}
