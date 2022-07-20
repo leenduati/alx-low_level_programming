b7ae194b74edeb12fba0e45a7163fa0484eec1f7
leenduati <leenduati21@gmail.com>
2022-07-20 23:55:57 +0000

    Print a string in reverse using recursive method

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,28 +1,16 @@
 #include "main.h"
-#include <string.h>
 
 /**
- *_puts_recursion -  prints string via recrsive function
- *@s: A pointer to an char that will be updated from src
- *Return: 0
+ * _print_rev_recursion - reverse
+ * @s: pointer to string params
+ *
  */
-
 
 void _print_rev_recursion(char *s)
 {
-	/* Base Case */
-
-
-	if (*s == '\n')
+	if (*s)
 	{
-		_putchar('l');
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-
-	/* End of base case */
-
-	_putchar(s[a]);
-
-	_print_rev_recursion(s + 1);
-
 }
