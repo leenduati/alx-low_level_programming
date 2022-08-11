5d69eba58943a5e7b7b6029a5ff2359415a531f3
leenduati <leenduati21@gmail.com>
2022-08-11 22:55:04 +0000

    Functions that append and push elements to list_links

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,19 +4,16 @@
 #include "lists.h"
 
 /**
- *print_list-function that prints all the elements of a list_t list.
- *@h:this is a pointer that is defined by the structure declared in lists.h
+ *add_node_end-function that pushes last element to alinked list.
+ *@head:this is a pointer that is defined by the structure declared in lists.h
+ *@str: this is the string thst ewilll be pushed
  *Return: Return a unsigned int which is a size_t(number of nodes)
  *int l = 0;
  *int i = 0;
  *Declared a size_t length which will store length of the strings
  *int i which is the loop integer
  *count which will count the nodes form 1
-*/
-
-
-/**
- head = NULL
+ *head = NULL
  *add_node(&head, "Alexandro");
  *add_node(&head, "Asaia");
  *add_node(&head, "Augustin");
@@ -34,6 +31,12 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	while (str[length])
 		length++;
+
+	/**
+	 * for pushing to last elem, we need first to allocate temp.str to the strcopy
+	 * then assigning head mem all to vary of temp
+	 * at each instance, we will add/push new node to the end
+	*/
 
 	temp->len = length;
 	temp->str = strdup(str);
