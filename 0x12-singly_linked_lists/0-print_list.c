091b1a4273ad7f78ecfcca715e94d45b97f5ece5
leenduati <leenduati21@gmail.com>
2022-08-11 18:31:46 +0000

    Fixed the requirements in the code task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,50 +7,47 @@
  *print_list-function that prints all the elements of a list_t list.
  *@h:this is a pointer that is defined by the structure declared in lists.h
  *Return: Return a unsigned int which is a size_t(number of nodes)
+ *int l = 0;
+ *int i = 0;
+ *Declared a size_t length which will store length of the strings
+ *int i which is the loop integer
+ *count which will count the nodes form 1
 */
-
 
 
 size_t print_list(const list_t *h)
 {
-	size_t l;
-	int i = 0;
 	int count = 1;
-
-	/*
-	 * Declared a size_t length which will store length of the strings
-	 * int i which is the loop integer
-	 * count which will count the nodes form 1
-	 */
 
 	if (h->str == NULL)
 	{
 		/**
 		 *if string is null, print null then assign h to next value
 		 */
-
 		printf("[%d] (nil)\n", 0);
 		h = h->next;
 		count++;
 	}
-	while (h->str[i] != '\0')
+	/*
+	 *while (h->str[i] != '\0')
+	 *{
+		* l++;
+		* i++;
+	 * }
+	 */
+	if (h != NULL)
 	{
-		l++;
-		i++;
-	}
-
-	printf("[%d] %s\n", h->len, h->str);
-
-	while (h->next != NULL)
-	{
-		h = h->next;
 		printf("[%d] %s\n", h->len, h->str);
-		count++;
-	}
 
+		while (h->next != NULL)
+		{
+			h = h->next;
+			printf("[%d] %s\n", h->len, h->str);
+			count++;
+		}
+	}
 	/*
 	 * printf("[%ld] %s\n", len, h->str);
-	*/
-
+	 */
 	return (count);
 }
