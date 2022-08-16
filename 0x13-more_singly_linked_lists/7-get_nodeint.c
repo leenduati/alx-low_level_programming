b1bcad16cd8366f188e9e3e1d0c80ab8a52dfd67
leenduati <leenduati21@gmail.com>
2022-08-16 08:45:40 +0000

    Edited task 7 to return node at that instance

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,23 +3,47 @@
 #include <stdio.h>
 #include "lists.h"
 
+
+/**
+ *get_nodeint_at_index-returns the nth node of a listint_t linked list.
+ *@head: this is the list given
+ *@index: index that is needed
+ *Return: return the list at that particular index
+*/
+
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *temp;
+	unsigned int count = 0;
+	listint_t *temp, *res;
 
 	temp = malloc(sizeof(listint_t));
+	res = malloc(sizeof(listint_t));
 
-	if (head == NULL || index < 0)
+	if (temp == NULL)
 	{
 		return (NULL);
 	}
+	temp = head;
+	res = head;
 
-	while (i <= index)
+	while (res->next != NULL)
 	{
-		temp = head->next;
+		res = res->next;
+		count++;
+	}
+
+	if (index > count)
+	{
+		temp = NULL;
+	}
+	while (temp->next != NULL && i < index)
+	{
+		temp = temp->next;
 		i++;
 	}
 	return (temp);
+	free(temp);
 }
 
