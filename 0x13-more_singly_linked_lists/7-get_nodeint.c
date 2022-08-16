9931556aaff57ac44aa58a7770c8f40e279fa0f5
leenduati <leenduati21@gmail.com>
2022-08-16 18:41:05 +0000

    Edited task 7 to pass requirements and failed check

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,33 +15,15 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	unsigned int count = 0;
-	listint_t *temp, *res;
+	listint_t *temp = head;
 
-	temp = malloc(sizeof(listint_t));
-	if (temp == NULL)
+	while (temp && i < index)
 	{
-		return (NULL);
-	}
-	temp = head;
-	res = head;
-
-	while (res->next != NULL)
-	{
-		res = res->next;
-		count++;
+	temp = temp->next;
+	i++;
 	}
 
-	while (temp->next != NULL && i < index)
-	{
-		temp = temp->next;
-		i++;
-	}
-
-	if (temp && count >= index)
-		return (temp);
-	else
-		return (NULL);
-	free(temp);
+	return (temp ? temp : NULL);
 }
+
 
