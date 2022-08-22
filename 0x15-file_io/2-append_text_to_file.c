4deae27900e737f57afd6b09020ea3f816ff5bcb
leenduati <leenduati21@gmail.com>
2022-08-22 22:00:23 +0000

    Added requirement to check if text_context is NULL and reassign var tempty string

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -28,9 +28,6 @@ int create_file(const char *filename, char *text_context)
 	if (fd == -1)
 		return (-1);
 
-	if (text_context == NULL)
-		text_context = "";
-
 	k = write(fd, text_context, strlen(text_context));
 
 	if (k == -1)
