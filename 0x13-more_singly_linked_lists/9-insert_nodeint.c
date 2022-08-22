2e640fee917f6730030a31533dcca89650ba8189
leenduati <leenduati21@gmail.com>
2022-08-22 06:48:49 +0000

    Final updates

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,24 +3,45 @@
 #include <stdio.h>
 #include "lists.h"
 
+
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+ *get_nodeint_at_index-returns the nth node of a listint_t linked list.
+ *@head: this is the list given
+ *@index: index that is needed
+ *Return: return the list at that particular index
+*/
+
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp;
 	unsigned int i = 0;
+	unsigned int count = 0;
+	listint_t *temp, *res;
 
 	temp = malloc(sizeof(listint_t));
-	while (i < idx)
+	if (temp == NULL)
 	{
-		temp = *head;
-		temp->n = (*head)->n;
-		temp->next = *head;
+		return (NULL);
+	}
+	temp = *head;
+	res = *head;
+
+	while (res->next != NULL)
+	{
+		res = res->next;
+		count++;
+	}
+
+	while (temp != NULL && i < idx)
+	{
+		temp = temp->next;
 		i++;
 	}
-	return (temp);
+
+	if (temp && count >= idx)
+		return (temp);
+	else
+		return (NULL);
+	free(temp);
 }
+
