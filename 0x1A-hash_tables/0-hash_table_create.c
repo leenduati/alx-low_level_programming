ddfccfeee3d8c942ac35cdf67b6e46f7df299edd
leenduati <leenduati21@gmail.com>
2022-10-23 15:54:59 -0400

    task 1, hash tables and readme

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ *hash_table_create- creates a hash_table
+ *@size: size ofthe array
+ *Return: returns a hash_table
+**/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	hash_node_t **temp;
+	hash_table_t *h;
+
+	temp = malloc(sizeof(hash_node_t));
+	h = malloc(sizeof(hash_table_t));
+
+	h->size = size;
+	h->array = temp;
+	return (h);
+	free(temp);
+	free(h);
+}
